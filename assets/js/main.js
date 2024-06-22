f59fd71e0ccbfc0b35ef14702927b895600a40f5
FarruXbek Raxmonov <raxmonovapple@gmail.com>
2024-06-22 15:50:17 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById( 'nav-menu'),
+        navToggle = document.getElementById( 'nav-toggle'),
+        navClose = document.getElementById( 'nav-close')
+
+        
+// Menu Show
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+// Menu hidden
+
+if(navClose) {
+    navClose.addEventListener('click', ()=>{
+        navMenu. classList. remove('show-menu')
+})
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('clic', linkAction))
+
+/*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 30 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header') 
+
+}
+window.addEventListener('scroll', shadowHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+
+}
+
+window.addEventListener('scroll' , scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+
+    })
+}
+
+
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
